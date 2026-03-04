03f51f33b10ec255c08e58eaf27ca73240cb9157
YU-WEI,HSU <weiso131@weiso131.com>
2026-03-04 15:32:49 +0800

    Add newline detection in update_char_mem
    Previously, when a string wrapped due to excessive length,
    oled_prev_line could not update oled_show_end to the expected
    value, resulting in display errors.
    
    To resolve this issue, a counter has been added in
    update_char_mem. If no newline is encountered after 16
    consecutive characters, a newline marker is automatically
    appended to self->mem.
    
    This allows oled_prev_line to correctly determine the proper
    oled_show_end.
    
    Signed-off-by: YU-WEI,HSU <weiso131@weiso131.com>

--- src/char_mem.c
+++ src/char_mem.c
@@ -1,11 +1,19 @@
 #include <char_mem.h>
 
+char char_cnt = 0;
+
 void update_char_mem(char_mem_t *self, char c)
 {
-    if (c == '\n')
+    if (c == '\n') {
         self->mem[(self->end_ptr + CHAR_MEMORY_NUM - 1) % CHAR_MEMORY_NUM] |=
             CHAR_MEMORY_NEWLINE;
-    else if (c >= 0x20 && c <= 0x7E) {
+        char_cnt = 0;
+    } else if (c >= 0x20 && c <= 0x7E) {
+        char_cnt++;
+        if (char_cnt == 16) {
+            char_cnt = 0;
+            c |= CHAR_MEMORY_NEWLINE;
+        }
         self->mem[self->end_ptr] = c;
         self->end_ptr = (self->end_ptr + 1) % CHAR_MEMORY_NUM;
         if (self->end_ptr == self->start_ptr)
